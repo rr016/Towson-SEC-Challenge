e7aaf1ab05791acc0ee9af6e47d1bf0f33095fdd
rr016 <rafr912@gmail.com>
2019-10-31 20:00:54 -0400

    Front-end work

--- client/src/components/SplashPage.jsx
+++ client/src/components/SplashPage.jsx
@@ -7,9 +7,24 @@ import './Main.css';
 class SplashPage extends Component {
     render() {
         return (
-            <div className="container">
-                <div className="back-image">
-                    <p id="test">Hello World!</p>
+            <div className="image-background">
+                <div className="container">
+
+                    <div id="top-buttons-container">
+                        <button id="button-contact" className="btn-type1">Contact Us</button>
+                        <button id="button-events" className="btn-type1">View Events</button>
+                    </div>
+
+                    <div id="center-container">
+                        <p>We are</p>
+                        <p>SEC@ <img className="image-TU-logo" src={require("./img/TowsonUlogo.png")} alt="Towson University logo"/></p>
+                        <p>Together we build great software</p>
+                    </div>
+
+                    <div id="bottom-button-container">
+                        <button id="button-learn" className="btn-type2">Learn More</button>
+                    </div>
+
                 </div>      
             </div>     
         );
