0f050bdb764067313e9b01a254dd920e8fc26fb0
rr016 <rafr912@gmail.com>
2019-11-05 13:03:50 -0500

    Splash page complete

--- client/src/components/SplashPage.jsx
+++ client/src/components/SplashPage.jsx
@@ -7,26 +7,27 @@ import './Main.css';
 class SplashPage extends Component {
     render() {
         return (
-            <div className="image-background">
-                <div className="container">
-
+            <div>
+                <div id="left-side" className="column">
+                    <img className="image-SEC-club" src={require("./img/asset1.png")} alt="SEC meeting"/>
+                </div> 
+                <div id="right-side" className="column">
                     <div id="top-buttons-container">
                         <button id="button-contact" className="btn-type1">Contact Us</button>
                         <button id="button-events" className="btn-type1">View Events</button>
                     </div>
 
                     <div id="center-container">
-                        <p>We are</p>
-                        <p>SEC@ <img className="image-TU-logo" src={require("./img/TowsonUlogo.png")} alt="Towson University logo"/></p>
-                        <p>Together we build great software</p>
+                        <p className="center-text-bold">We are</p>
+                        <div><span className="center-text-bold">SEC@</span> <img className="image-TU-logo" src={require("./img/TowsonUlogo.png")} alt="TU logo"/> </div>
+                        <p className="center-text-weak">Together we build great software</p>
                     </div>
 
                     <div id="bottom-button-container">
                         <button id="button-learn" className="btn-type2">Learn More</button>
                     </div>
-
-                </div>      
-            </div>     
+                </div>
+            </div>
         );
     }
 }
